9604971fec01ec22dff5fe1ada8206bf282fd960
afik <afikziv@gmail.com>
2021-08-01 12:46:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// jshint esversion:6
+
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+client.setConfig({
+  apiKey: "04ef017c1f555424c2b91908e9048afe-us6",
+  server: "us6",
+});
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+
+  const run = async () => {
+    try {
+      const response = await client.lists.addListMember("0c64b29a59", {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+        }
+      });
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+
+  run();
+});
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000.");
+});
